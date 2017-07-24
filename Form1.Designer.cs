6517e5df3d781bb7f5ca6946b53629dfa4171198
Lucien <1104312113@gm.kuas.edu.tw>
2017-07-24 17:34:26 +0800

    profile

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace horse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.run = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.move = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.Interval = 10;
+            this.run.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "跑";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(0, 35);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(180, 180);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PictureBox1.TabIndex = 1;
+            this.PictureBox1.TabStop = false;
+            // 
+            // move
+            // 
+            this.move.Interval = 10;
+            this.move.Tick += new System.EventHandler(this.move_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 348);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer run;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Timer move;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace horse
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        int imgName = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            imgName++;
+            PictureBox1.Image = Image.FromFile(@"D:\horse\"+ imgName.ToString()+ ".PNG");
+            if (imgName > 15)
+                imgName =1;
+          
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            run.Enabled = true;
+            move.Enabled = true;
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int imgName = 1;
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            run.Interval = 50;
+            move.Interval = 50;
+
+            
+
+        }
+
+        private void move_Tick(object sender, EventArgs e)
+        {
+            PictureBox1.Left += 20;
+            if (PictureBox1.Left >= 1000)
+                PictureBox1.Left = 0;
+                
+        }
+    }
+}
